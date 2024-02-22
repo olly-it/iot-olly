ea4116dbf8924bdd9a173a8c03428852da9cf4a6
Alessio Olivieri <alessio.olivieri@enginius.com>
2024-02-22 23:21:21 +0100

    moved firebase_config inside firebase_service

--- esp32-nodejs-api/src/firebase.config.js
+++
@@ -1,9 +0,0 @@
-// Your web app's Firebase configuration
-export const firebaseConfig = {
-    apiKey: process.env.fb_apiKey,
-    authDomain: process.env.fb_authDomain,
-    projectId: process.env.fb_projectId,
-    storageBucket: process.env.fb_storageBucket,
-    messagingSenderId: process.env.fb_messagingSenderId,
-    appId: process.env.fb_appId
-};
--- esp32-nodejs-api/src/firebase.service.js
+++ esp32-nodejs-api/src/firebase.service.js
@@ -3,14 +3,24 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 import {
     getFirestore,
     query,
     collection,
     getDocs
-  } from 'firebase/firestore';
+} from 'firebase/firestore';
 
-import { firebaseConfig } from "./firebase.config.js";
+const firebaseConfig = {
+    apiKey: process.env.fb_apiKey,
+    authDomain: process.env.fb_authDomain,
+    projectId: process.env.fb_projectId,
+    storageBucket: process.env.fb_storageBucket,
+    messagingSenderId: process.env.fb_messagingSenderId,
+    appId: process.env.fb_appId
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
